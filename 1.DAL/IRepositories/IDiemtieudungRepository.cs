9c0a22a332aef4cdb29b91399b68a8781d8f1347
duythai010305@gmail.com <duythai010305@gmail.com>
2022-11-13 13:20:31 +0700

    repository(diem tieu dung, lich su tieu va dung )

---
+++ 1.DAL/IRepositories/IDiemtieudungRepository.cs
@@ -0,0 +1,17 @@
+﻿using _1.DAL.DomainClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.DAL.IRepositories
+{
+    public interface IDiemtieudungRepository
+    {
+        public bool Add(DiemTieuDung obj);
+        public bool Update(DiemTieuDung obj);
+        public bool Remove(DiemTieuDung obj);
+        public List<DiemTieuDung> GetAll();
+    }
+}
---
+++ 1.DAL/IRepositories/ILichsudiemdungRepository.cs
@@ -0,0 +1,17 @@
+﻿using _1.DAL.DomainClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.DAL.IRepositories
+{
+    public interface ILichsudiemdungRepository
+    {
+        public bool Add(LichSuDiemDung obj);
+        public bool Update(LichSuDiemDung obj);
+        public bool Remove(LichSuDiemDung obj);
+        public List<LichSuDiemDung> GetAll();
+    }
+}
