534261276839d02e0daabf15b5066a9758437f82
duythai010305@gmail.com <duythai010305@gmail.com>
2022-11-13 15:46:52 +0700

    update repository

---
+++ 1.DAL/Repositories/DiemtieudungRepository.cs
@@ -0,0 +1,55 @@
+﻿using _1.DAL.Context;
+using _1.DAL.DomainClass;
+using _1.DAL.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.DAL.Repositories
+{
+    public class DiemtieudungRepository : IDiemtieudungRepository
+    {
+        public FpolyDBContext _DbContext  = new FpolyDBContext();
+        public DiemtieudungRepository()
+        {
+
+        }
+        public bool Add(DiemTieuDung obj)
+        {
+            if (obj == null) return false;
+            _DbContext.DiemTieuDungs.Add(obj);
+            _DbContext.SaveChanges();
+            return true;
+        }
+
+        public List<DiemTieuDung> GetAll()
+        {
+            return _DbContext.DiemTieuDungs.ToList();
+        }
+
+        public bool Remove(DiemTieuDung obj)
+        {
+            if (obj == null) return false;
+            var tempobj = _DbContext.DiemTieuDungs.FirstOrDefault(x => x.Id == obj.Id);
+            tempobj.TrangThai = 0;
+            _DbContext.DiemTieuDungs.Update(tempobj);
+            _DbContext.SaveChanges();
+            return true;
+        }
+
+        public bool Update(DiemTieuDung obj)
+        {
+            if (obj == null) return false;
+            var tempobj = _DbContext.DiemTieuDungs.FirstOrDefault(x => x.Id == obj.Id);
+            tempobj.Ma = obj.Ma;
+            tempobj.SoDiem = obj.SoDiem;
+            tempobj.IdKh = obj.IdKh;
+            tempobj.TrangThai = obj.TrangThai;
+            _DbContext.DiemTieuDungs.Update(tempobj);
+            _DbContext.SaveChanges();
+            return true;
+        }
+    }
+}
---
+++ 1.DAL/Repositories/LichSuDiemDungRepository.cs
@@ -0,0 +1,57 @@
+﻿using _1.DAL.Context;
+using _1.DAL.DomainClass;
+using _1.DAL.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.DAL.Repositories
+{
+    public class LichSuDiemDungRepository: ILichsudiemdungRepository
+    {
+        public FpolyDBContext _DbContext = new FpolyDBContext();
+        public LichSuDiemDungRepository()
+        {
+
+        }
+        public bool Add(LichSuDiemDung obj)
+        {
+            if (obj == null) return false;
+            _DbContext.LichSuDiemDungs.Add(obj);
+            _DbContext.SaveChanges();
+            return true;
+        }
+
+        public List<LichSuDiemDung> GetAll()
+        {
+            return _DbContext.LichSuDiemDungs.ToList();
+        }
+
+        public bool Remove(LichSuDiemDung obj)
+        {
+            if (obj == null) return false;
+            var tempobj = _DbContext.LichSuDiemDungs.FirstOrDefault(x => x.Id == obj.Id);
+            tempobj.TrangThai = 0;
+            _DbContext.LichSuDiemDungs.Update(tempobj);
+            _DbContext.SaveChanges();
+            return true;
+        }
+
+        public bool Update(LichSuDiemDung obj)
+        {
+            if (obj == null) return false;
+            var tempobj = _DbContext.LichSuDiemDungs.FirstOrDefault(x => x.Id == obj.Id);
+            tempobj.IdCttinh = obj.IdCttinh;
+            tempobj.Ma = obj.Ma;
+            tempobj.SoDiemDung = obj.SoDiemDung;
+            tempobj.NgaySuDung = obj.NgaySuDung;
+            tempobj.TongTien = obj.TongTien;          
+            tempobj.TrangThai = obj.TrangThai;
+            _DbContext.LichSuDiemDungs.Update(tempobj);
+            _DbContext.SaveChanges();
+            return true;
+        }
+    }
+}
