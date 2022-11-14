e7da26bc4fc36476853ae3224714e40f49029165
DuyTuan25 <tuanndph25862@fpt.edu.vn>
2022-11-14 23:41:18 +0700

    1234

--- 3.PresentationLayers/Program.cs
+++ 3.PresentationLayers/Program.cs
@@ -18,7 +18,7 @@ namespace _3.PresentationLayers
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmChiTietTheLoai());
+            Application.Run(new Form_NhanVien());
         }
     }
 }
