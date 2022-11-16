c8a4e99f6a23981caa350b8baa98f8864df40f33
Nguyễn Văn Thắng <thangnguyen1632003@gmail.com>
2022-11-16 22:19:44 +0700

    Form_BanHang

---
+++ 3.PresentationLayers/Views/Form_BanHang.Designer.cs
@@ -0,0 +1,372 @@
+﻿namespace _3.PresentationLayers.Views
+{
+    partial class Form_BanHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_BanHang));
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lb_HoaDonChiTiet = new System.Windows.Forms.Label();
+            this.dtg_HoaDonChiTiet = new System.Windows.Forms.DataGridView();
+            this.lb_SanPham = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dtg_SanPham = new System.Windows.Forms.DataGridView();
+            this.btn_taohoadon = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_HoaDonChiTiet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_SanPham)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button5
+            // 
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.Location = new System.Drawing.Point(935, 485);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(127, 50);
+            this.button5.TabIndex = 23;
+            this.button5.Text = "Hủy ";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.Location = new System.Drawing.Point(716, 485);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 50);
+            this.button4.TabIndex = 22;
+            this.button4.Text = "Thanh Toán";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // lb_HoaDonChiTiet
+            // 
+            this.lb_HoaDonChiTiet.AutoSize = true;
+            this.lb_HoaDonChiTiet.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lb_HoaDonChiTiet.Location = new System.Drawing.Point(47, 15);
+            this.lb_HoaDonChiTiet.Name = "lb_HoaDonChiTiet";
+            this.lb_HoaDonChiTiet.Size = new System.Drawing.Size(144, 23);
+            this.lb_HoaDonChiTiet.TabIndex = 41;
+            this.lb_HoaDonChiTiet.Text = "Hóa đơn chi tiết";
+            // 
+            // dtg_HoaDonChiTiet
+            // 
+            this.dtg_HoaDonChiTiet.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtg_HoaDonChiTiet.Location = new System.Drawing.Point(47, 38);
+            this.dtg_HoaDonChiTiet.Name = "dtg_HoaDonChiTiet";
+            this.dtg_HoaDonChiTiet.RowHeadersWidth = 51;
+            this.dtg_HoaDonChiTiet.RowTemplate.Height = 29;
+            this.dtg_HoaDonChiTiet.Size = new System.Drawing.Size(482, 441);
+            this.dtg_HoaDonChiTiet.TabIndex = 40;
+            // 
+            // lb_SanPham
+            // 
+            this.lb_SanPham.AutoSize = true;
+            this.lb_SanPham.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lb_SanPham.Location = new System.Drawing.Point(47, 525);
+            this.lb_SanPham.Name = "lb_SanPham";
+            this.lb_SanPham.Size = new System.Drawing.Size(91, 23);
+            this.lb_SanPham.TabIndex = 44;
+            this.lb_SanPham.Text = "Sản phẩm";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(172, 525);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 27);
+            this.textBox1.TabIndex = 43;
+            this.textBox1.Text = "Tìm kiếm sản phẩm";
+            // 
+            // dtg_SanPham
+            // 
+            this.dtg_SanPham.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtg_SanPham.Location = new System.Drawing.Point(21, 554);
+            this.dtg_SanPham.Name = "dtg_SanPham";
+            this.dtg_SanPham.RowHeadersWidth = 51;
+            this.dtg_SanPham.RowTemplate.Height = 29;
+            this.dtg_SanPham.Size = new System.Drawing.Size(1120, 248);
+            this.dtg_SanPham.TabIndex = 42;
+            // 
+            // btn_taohoadon
+            // 
+            this.btn_taohoadon.FlatAppearance.BorderSize = 0;
+            this.btn_taohoadon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_taohoadon.Image = ((System.Drawing.Image)(resources.GetObject("btn_taohoadon.Image")));
+            this.btn_taohoadon.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_taohoadon.Location = new System.Drawing.Point(602, 15);
+            this.btn_taohoadon.Name = "btn_taohoadon";
+            this.btn_taohoadon.Size = new System.Drawing.Size(41, 45);
+            this.btn_taohoadon.TabIndex = 46;
+            this.btn_taohoadon.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_taohoadon.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(247, 0);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(660, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(255, 24);
+            this.tabControl1.TabIndex = 47;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(1016, 104);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(79, 24);
+            this.radioButton3.TabIndex = 71;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Đã hủy";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(862, 104);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(138, 24);
+            this.radioButton2.TabIndex = 70;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Chờ thanh toán";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(716, 104);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(130, 24);
+            this.radioButton1.TabIndex = 69;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Đã thanh toán";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(625, 104);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(80, 20);
+            this.label8.TabIndex = 68;
+            this.label8.Text = "Trạng thái";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(633, 359);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 20);
+            this.label6.TabIndex = 67;
+            this.label6.Text = "Tiền trả lại";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(749, 356);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(249, 27);
+            this.textBox6.TabIndex = 66;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(749, 143);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(249, 27);
+            this.textBox2.TabIndex = 56;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(749, 311);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(249, 27);
+            this.textBox7.TabIndex = 64;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(749, 188);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(249, 27);
+            this.textBox3.TabIndex = 58;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(749, 268);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(249, 27);
+            this.textBox4.TabIndex = 62;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(749, 227);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(249, 27);
+            this.textBox5.TabIndex = 60;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(619, 318);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(115, 20);
+            this.label7.TabIndex = 65;
+            this.label7.Text = "Tiền khách đưa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(625, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 57;
+            this.label2.Text = "Nhân viên";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(628, 275);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 20);
+            this.label4.TabIndex = 63;
+            this.label4.Text = "Tổng tiền";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(625, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 20);
+            this.label3.TabIndex = 59;
+            this.label3.Text = "Khách hàng";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(631, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 20);
+            this.label5.TabIndex = 61;
+            this.label5.Text = "Ngày tạo";
+            // 
+            // Form_BanHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1205, 825);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.btn_taohoadon);
+            this.Controls.Add(this.lb_SanPham);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dtg_SanPham);
+            this.Controls.Add(this.lb_HoaDonChiTiet);
+            this.Controls.Add(this.dtg_HoaDonChiTiet);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form_BanHang";
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_HoaDonChiTiet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_SanPham)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label lb_HoaDonChiTiet;
+        private System.Windows.Forms.DataGridView dtg_HoaDonChiTiet;
+        private System.Windows.Forms.Label lb_SanPham;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dtg_SanPham;
+        private System.Windows.Forms.Button btn_taohoadon;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ 3.PresentationLayers/Views/Form_BanHang.cs
@@ -0,0 +1,78 @@
+﻿using _1.DAL.IRepositories;
+using _2.BUS.IService;
+using _2.BUS.IServices;
+using _2.BUS.Serivces;
+using _2.BUS.Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3.PresentationLayers.Views
+{
+    public partial class Form_BanHang : Form
+    {
+        private IHoaDonService hoaDonService;
+        private IChiTietSachService chiTietSachService;
+
+        private ISachService sachService;
+        private IHoaDonChiTietService hoaDonChiTietService;
+        
+        public Form_BanHang()
+        {
+            InitializeComponent();
+            hoaDonService = new HoaDonService();
+            chiTietSachService = new ChiTietSachService();
+            sachService = new SachService();
+            hoaDonChiTietService = new HoaDonChiTietService();
+        }
+
+
+        public void LoadSach()
+        {
+            int i=0;
+            dtg_SanPham.ColumnCount = 18;
+            dtg_SanPham.Columns[0].Name = "Stt";
+            dtg_SanPham.Columns[1].Name = "ID";
+            dtg_SanPham.Columns[1].Visible = false;
+            dtg_SanPham.Columns[2].Name = "Sách";
+            dtg_SanPham.Columns[3].Name = "Nhà xuất bản";
+            dtg_SanPham.Columns[4].Name = "Tác giả";
+            dtg_SanPham.Columns[5].Name = "Nhà phát hành";
+            dtg_SanPham.Columns[6].Name = "Loại bìa";
+            dtg_SanPham.Columns[7].Name = "Mã";
+            dtg_SanPham.Columns[8].Name = "";
+            dtg_SanPham.Columns[9].Name = "Stt";
+            dtg_SanPham.Columns[10].Name = "Stt";
+            dtg_SanPham.Columns[11].Name = "Stt";
+            dtg_SanPham.Columns[12].Name = "Stt";
+            dtg_SanPham.Columns[13].Name = "Stt";
+            dtg_SanPham.Columns[14].Name = "Stt";
+            dtg_SanPham.Columns[15].Name = "Stt";
+            dtg_SanPham.Columns[16].Name = "Stt";
+            dtg_SanPham.Columns[17].Name = "Stt";
+            
+            
+        }
+
+        public void LoadHoaDon()
+        {
+            
+
+        }
+
+        public void LoadHoaDonChiTiet()
+        {
+            int i = 1;
+            dtg_HoaDonChiTiet.ColumnCount = 10;
+            dtg_HoaDonChiTiet.Columns[0].Name = "Stt";
+        }
+
+      
+    }
+}
