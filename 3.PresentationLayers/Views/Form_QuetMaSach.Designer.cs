cafdd20f3d6d9cb299165b5977cb37a2ef0beb24
DuyTuan25 <tuanndph25862@fpt.edu.vn>
2022-11-26 18:19:11 +0700

    chuc nang quet barcode

---
+++ 3.PresentationLayers/Views/Form_QuetMaSach.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace _3.PresentationLayers.Views
+{
+    partial class Form_QuetMaSach
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbb_tenmay = new System.Windows.Forms.ComboBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.tb_mavach = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ptb_barcode = new System.Windows.Forms.PictureBox();
+            this.tb_tensach = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_barcode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbb_tenmay
+            // 
+            this.cbb_tenmay.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbb_tenmay.FormattingEnabled = true;
+            this.cbb_tenmay.Location = new System.Drawing.Point(99, 30);
+            this.cbb_tenmay.Name = "cbb_tenmay";
+            this.cbb_tenmay.Size = new System.Drawing.Size(268, 28);
+            this.cbb_tenmay.TabIndex = 17;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(666, 441);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(122, 29);
+            this.btn_add.TabIndex = 16;
+            this.btn_add.Text = "Thêm sách";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // tb_mavach
+            // 
+            this.tb_mavach.Location = new System.Drawing.Point(99, 394);
+            this.tb_mavach.Name = "tb_mavach";
+            this.tb_mavach.Size = new System.Drawing.Size(561, 27);
+            this.tb_mavach.TabIndex = 15;
+            this.tb_mavach.TextChanged += new System.EventHandler(this.tb_mavach_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 397);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Mã vạch";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Camera";
+            // 
+            // ptb_barcode
+            // 
+            this.ptb_barcode.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ptb_barcode.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ptb_barcode.Location = new System.Drawing.Point(12, 64);
+            this.ptb_barcode.Name = "ptb_barcode";
+            this.ptb_barcode.Size = new System.Drawing.Size(776, 309);
+            this.ptb_barcode.TabIndex = 12;
+            this.ptb_barcode.TabStop = false;
+            // 
+            // tb_tensach
+            // 
+            this.tb_tensach.Location = new System.Drawing.Point(99, 442);
+            this.tb_tensach.Name = "tb_tensach";
+            this.tb_tensach.ReadOnly = true;
+            this.tb_tensach.Size = new System.Drawing.Size(561, 27);
+            this.tb_tensach.TabIndex = 23;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 445);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Tên sách";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(666, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 29);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "Quét lại";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form_QuetMaSach
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 483);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb_tensach);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbb_tenmay);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.tb_mavach);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ptb_barcode);
+            this.Name = "Form_QuetMaSach";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form_QuetMaSach";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_QuetMaSach_FormClosing);
+            this.Load += new System.EventHandler(this.Form_QuetMaSach_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_barcode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbb_tenmay;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.TextBox tb_mavach;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox ptb_barcode;
+        private System.Windows.Forms.TextBox tb_tensach;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ 3.PresentationLayers/Views/Form_QuetMaSach.cs
@@ -0,0 +1,125 @@
+﻿using _2.BUS.IService;
+using _2.BUS.Service;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace _3.PresentationLayers.Views
+{
+    public partial class Form_QuetMaSach : Form
+    {
+        IChiTietSachService _ichiTietSachService;
+        ISachService _iSachService;
+        public Form_QuetMaSach()
+        {
+            InitializeComponent();
+            _ichiTietSachService = new ChiTietSachService();
+            _iSachService = new SachService();
+        }
+        private FilterInfoCollection filterInfoCollection;
+        private VideoCaptureDevice videoCaptureDevice;
+        public static string mavach;
+        public static string tensach;
+        private void Lammoi()
+        {
+            try
+            {
+                if (InvokeRequired)
+                {
+                    this.Invoke(new MethodInvoker(Lammoi));
+                    return;
+                }
+                if (ptb_barcode.Image != null)
+                {
+                    ptb_barcode.Image = null;
+                    ptb_barcode.ImageLocation = null;
+                    //pic_cam.Image = null;
+                    ptb_barcode.Update();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(Convert.ToString(ex), "123");
+                return;
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tb_mavach.Text = "";
+            tb_tensach.Text = "";
+            Lammoi();
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            mavach = tb_mavach.Text;
+            tensach= tb_tensach.Text;
+            this.Close();
+        }
+
+        private void Form_QuetMaSach_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in filterInfoCollection)
+            {
+                cbb_tenmay.Items.Add(device.Name);
+            }
+            cbb_tenmay.SelectedIndex = 0;
+            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cbb_tenmay.SelectedIndex].MonikerString);
+            videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            videoCaptureDevice.Start();
+        }
+
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            BarcodeReader reader = new BarcodeReader();
+            var result = reader.Decode(bitmap);
+            if (result != null)
+            {
+                tb_mavach.Invoke(new MethodInvoker(delegate ()
+                {
+                    tb_mavach.Text = result.ToString();
+                    
+                }));
+            }
+            ptb_barcode.Image = bitmap;
+        }
+
+        private void Form_QuetMaSach_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (videoCaptureDevice != null)
+                {
+                    if (videoCaptureDevice.IsRunning == true) videoCaptureDevice.SignalToStop();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void tb_mavach_TextChanged(object sender, EventArgs e)
+        {
+
+                string x = _ichiTietSachService.GetAllChiTietSachView().Where(c => c.MaVach == tb_mavach.Text).Select(x => x.TenSach).FirstOrDefault();
+                tb_tensach.Text = x;
+            
+        }
+    }
+}
