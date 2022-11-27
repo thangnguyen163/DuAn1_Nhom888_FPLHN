0029180dc1dd5d9f6afaa5ef643a53e696308f5b
Nguyễn Văn Thắng <thangnguyen1632003@gmail.com>
2022-11-27 21:35:56 +0700

    Update Design Dasboard

--- 3.PresentationLayers/Views/Form_Dasboard.Designer.cs
+++ 3.PresentationLayers/Views/Form_Dasboard.Designer.cs
@@ -36,9 +36,14 @@
             this.panelLogo = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panelMenu = new System.Windows.Forms.Panel();
-            this.logout = new System.Windows.Forms.Button();
-            this.btn_sanpham = new System.Windows.Forms.Button();
+            this.btn_LogOut = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.btn_ThongKe = new System.Windows.Forms.Button();
+            this.btn_KhuyenMai = new System.Windows.Forms.Button();
+            this.btn_KhachHang = new System.Windows.Forms.Button();
+            this.btn_NhanVien = new System.Windows.Forms.Button();
+            this.btn_HoaDon = new System.Windows.Forms.Button();
+            this.btn_sanpham = new System.Windows.Forms.Button();
             this.btn_shopping = new System.Windows.Forms.Button();
             this.PanelTitel.SuspendLayout();
             this.panelLogo.SuspendLayout();
@@ -82,7 +87,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.PanelDesktopPanel.Location = new System.Drawing.Point(228, 100);
             this.PanelDesktopPanel.Name = "PanelDesktopPanel";
-            this.PanelDesktopPanel.Size = new System.Drawing.Size(1158, 498);
+            this.PanelDesktopPanel.Size = new System.Drawing.Size(1158, 673);
             this.PanelDesktopPanel.TabIndex = 2;
             // 
             // panelLogo
@@ -109,52 +114,56 @@
             // panelMenu
             // 
             this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
-            this.panelMenu.Controls.Add(this.logout);
-            this.panelMenu.Controls.Add(this.btn_sanpham);
+            this.panelMenu.Controls.Add(this.btn_LogOut);
+            this.panelMenu.Controls.Add(this.button6);
             this.panelMenu.Controls.Add(this.btn_ThongKe);
+            this.panelMenu.Controls.Add(this.btn_KhuyenMai);
+            this.panelMenu.Controls.Add(this.btn_KhachHang);
+            this.panelMenu.Controls.Add(this.btn_NhanVien);
+            this.panelMenu.Controls.Add(this.btn_HoaDon);
+            this.panelMenu.Controls.Add(this.btn_sanpham);
             this.panelMenu.Controls.Add(this.btn_shopping);
             this.panelMenu.Controls.Add(this.panelLogo);
             this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelMenu.Location = new System.Drawing.Point(0, 0);
             this.panelMenu.Name = "panelMenu";
-            this.panelMenu.Size = new System.Drawing.Size(228, 598);
+            this.panelMenu.Size = new System.Drawing.Size(228, 773);
             this.panelMenu.TabIndex = 0;
             // 
-            // logout
+            // btn_LogOut
             // 
-            this.logout.Dock = System.Windows.Forms.DockStyle.Top;
-            this.logout.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
-            this.logout.FlatAppearance.BorderSize = 0;
-            this.logout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.logout.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.logout.ForeColor = System.Drawing.Color.White;
-            this.logout.Image = ((System.Drawing.Image)(resources.GetObject("logout.Image")));
-            this.logout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.logout.Location = new System.Drawing.Point(0, 250);
-            this.logout.Name = "logout";
-            this.logout.Size = new System.Drawing.Size(228, 60);
-            this.logout.TabIndex = 4;
-            this.logout.Text = "Thoát";
-            this.logout.UseVisualStyleBackColor = true;
-            this.logout.Click += new System.EventHandler(this.logout_Click);
+            this.btn_LogOut.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_LogOut.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btn_LogOut.FlatAppearance.BorderSize = 0;
+            this.btn_LogOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_LogOut.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_LogOut.ForeColor = System.Drawing.Color.White;
+            this.btn_LogOut.Image = ((System.Drawing.Image)(resources.GetObject("btn_LogOut.Image")));
+            this.btn_LogOut.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_LogOut.Location = new System.Drawing.Point(0, 660);
+            this.btn_LogOut.Name = "btn_LogOut";
+            this.btn_LogOut.Size = new System.Drawing.Size(228, 70);
+            this.btn_LogOut.TabIndex = 11;
+            this.btn_LogOut.Text = "Thoát";
+            this.btn_LogOut.UseVisualStyleBackColor = true;
+            this.btn_LogOut.Click += new System.EventHandler(this.btn_LogOut_Click);
             // 
-            // btn_sanpham
+            // button6
             // 
-            this.btn_sanpham.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn_sanpham.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
-            this.btn_sanpham.FlatAppearance.BorderSize = 0;
-            this.btn_sanpham.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_sanpham.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btn_sanpham.ForeColor = System.Drawing.Color.White;
-            this.btn_sanpham.Image = ((System.Drawing.Image)(resources.GetObject("btn_sanpham.Image")));
-            this.btn_sanpham.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btn_sanpham.Location = new System.Drawing.Point(0, 200);
-            this.btn_sanpham.Name = "btn_sanpham";
-            this.btn_sanpham.Size = new System.Drawing.Size(228, 50);
-            this.btn_sanpham.TabIndex = 3;
-            this.btn_sanpham.Text = "Sản Phẩm";
-            this.btn_sanpham.UseVisualStyleBackColor = true;
-            this.btn_sanpham.Click += new System.EventHandler(this.btn_sanpham_Click);
+            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button6.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.Location = new System.Drawing.Point(0, 590);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(228, 70);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Sản Phẩm";
+            this.button6.UseVisualStyleBackColor = true;
             // 
             // btn_ThongKe
             // 
@@ -166,13 +175,100 @@
             this.btn_ThongKe.ForeColor = System.Drawing.Color.White;
             this.btn_ThongKe.Image = ((System.Drawing.Image)(resources.GetObject("btn_ThongKe.Image")));
             this.btn_ThongKe.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btn_ThongKe.Location = new System.Drawing.Point(0, 150);
+            this.btn_ThongKe.Location = new System.Drawing.Point(0, 520);
             this.btn_ThongKe.Name = "btn_ThongKe";
-            this.btn_ThongKe.Size = new System.Drawing.Size(228, 50);
-            this.btn_ThongKe.TabIndex = 2;
+            this.btn_ThongKe.Size = new System.Drawing.Size(228, 70);
+            this.btn_ThongKe.TabIndex = 9;
             this.btn_ThongKe.Text = "Thống Kê";
             this.btn_ThongKe.UseVisualStyleBackColor = true;
-            this.btn_ThongKe.Click += new System.EventHandler(this.btn_ThongKe_Click);
+            // 
+            // btn_KhuyenMai
+            // 
+            this.btn_KhuyenMai.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_KhuyenMai.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btn_KhuyenMai.FlatAppearance.BorderSize = 0;
+            this.btn_KhuyenMai.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_KhuyenMai.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_KhuyenMai.ForeColor = System.Drawing.Color.White;
+            this.btn_KhuyenMai.Image = ((System.Drawing.Image)(resources.GetObject("btn_KhuyenMai.Image")));
+            this.btn_KhuyenMai.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_KhuyenMai.Location = new System.Drawing.Point(0, 450);
+            this.btn_KhuyenMai.Name = "btn_KhuyenMai";
+            this.btn_KhuyenMai.Size = new System.Drawing.Size(228, 70);
+            this.btn_KhuyenMai.TabIndex = 8;
+            this.btn_KhuyenMai.Text = "Khuyến Mãi";
+            this.btn_KhuyenMai.UseVisualStyleBackColor = true;
+            // 
+            // btn_KhachHang
+            // 
+            this.btn_KhachHang.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_KhachHang.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btn_KhachHang.FlatAppearance.BorderSize = 0;
+            this.btn_KhachHang.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_KhachHang.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_KhachHang.ForeColor = System.Drawing.Color.White;
+            this.btn_KhachHang.Image = ((System.Drawing.Image)(resources.GetObject("btn_KhachHang.Image")));
+            this.btn_KhachHang.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_KhachHang.Location = new System.Drawing.Point(0, 380);
+            this.btn_KhachHang.Name = "btn_KhachHang";
+            this.btn_KhachHang.Size = new System.Drawing.Size(228, 70);
+            this.btn_KhachHang.TabIndex = 7;
+            this.btn_KhachHang.Text = "Khách Hàng";
+            this.btn_KhachHang.UseVisualStyleBackColor = true;
+            this.btn_KhachHang.Click += new System.EventHandler(this.btn_KhachHang_Click);
+            // 
+            // btn_NhanVien
+            // 
+            this.btn_NhanVien.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_NhanVien.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btn_NhanVien.FlatAppearance.BorderSize = 0;
+            this.btn_NhanVien.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_NhanVien.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_NhanVien.ForeColor = System.Drawing.Color.White;
+            this.btn_NhanVien.Image = ((System.Drawing.Image)(resources.GetObject("btn_NhanVien.Image")));
+            this.btn_NhanVien.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_NhanVien.Location = new System.Drawing.Point(0, 310);
+            this.btn_NhanVien.Name = "btn_NhanVien";
+            this.btn_NhanVien.Size = new System.Drawing.Size(228, 70);
+            this.btn_NhanVien.TabIndex = 6;
+            this.btn_NhanVien.Text = "Nhân Viên";
+            this.btn_NhanVien.UseVisualStyleBackColor = true;
+            this.btn_NhanVien.Click += new System.EventHandler(this.btn_NhanVien_Click);
+            // 
+            // btn_HoaDon
+            // 
+            this.btn_HoaDon.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_HoaDon.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btn_HoaDon.FlatAppearance.BorderSize = 0;
+            this.btn_HoaDon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_HoaDon.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_HoaDon.ForeColor = System.Drawing.Color.White;
+            this.btn_HoaDon.Image = ((System.Drawing.Image)(resources.GetObject("btn_HoaDon.Image")));
+            this.btn_HoaDon.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_HoaDon.Location = new System.Drawing.Point(0, 240);
+            this.btn_HoaDon.Name = "btn_HoaDon";
+            this.btn_HoaDon.Size = new System.Drawing.Size(228, 70);
+            this.btn_HoaDon.TabIndex = 5;
+            this.btn_HoaDon.Text = "Hoá đơn";
+            this.btn_HoaDon.UseVisualStyleBackColor = true;
+            // 
+            // btn_sanpham
+            // 
+            this.btn_sanpham.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_sanpham.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btn_sanpham.FlatAppearance.BorderSize = 0;
+            this.btn_sanpham.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_sanpham.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_sanpham.ForeColor = System.Drawing.Color.White;
+            this.btn_sanpham.Image = ((System.Drawing.Image)(resources.GetObject("btn_sanpham.Image")));
+            this.btn_sanpham.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_sanpham.Location = new System.Drawing.Point(0, 170);
+            this.btn_sanpham.Name = "btn_sanpham";
+            this.btn_sanpham.Size = new System.Drawing.Size(228, 70);
+            this.btn_sanpham.TabIndex = 3;
+            this.btn_sanpham.Text = "Sản Phẩm";
+            this.btn_sanpham.UseVisualStyleBackColor = true;
+            this.btn_sanpham.Click += new System.EventHandler(this.btn_sanpham_Click);
             // 
             // btn_shopping
             // 
@@ -186,7 +282,7 @@
             this.btn_shopping.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btn_shopping.Location = new System.Drawing.Point(0, 100);
             this.btn_shopping.Name = "btn_shopping";
-            this.btn_shopping.Size = new System.Drawing.Size(228, 50);
+            this.btn_shopping.Size = new System.Drawing.Size(228, 70);
             this.btn_shopping.TabIndex = 1;
             this.btn_shopping.Text = "Bán Hàng";
             this.btn_shopping.UseVisualStyleBackColor = true;
@@ -197,7 +293,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1386, 598);
+            this.ClientSize = new System.Drawing.Size(1386, 773);
             this.Controls.Add(this.PanelDesktopPanel);
             this.Controls.Add(this.PanelTitel);
             this.Controls.Add(this.panelMenu);
@@ -221,9 +317,14 @@
         private System.Windows.Forms.Panel panelLogo;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panelMenu;
-        private System.Windows.Forms.Button logout;
         private System.Windows.Forms.Button btn_sanpham;
-        private System.Windows.Forms.Button btn_ThongKe;
         private System.Windows.Forms.Button btn_shopping;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button btn_ThongKe;
+        private System.Windows.Forms.Button btn_KhuyenMai;
+        private System.Windows.Forms.Button btn_KhachHang;
+        private System.Windows.Forms.Button btn_NhanVien;
+        private System.Windows.Forms.Button btn_HoaDon;
+        private System.Windows.Forms.Button btn_LogOut;
     }
 }
